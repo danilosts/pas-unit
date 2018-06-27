48173db296349745e4c0ba9bcdabc72031c50f3c
DaniloSouza <danilo.souza.ifs@gmail.com>
2018-06-27 17:57:31 -0300

    Fix server

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import bodyParser from 'body-parser';
 import MongoDb from './data/MongoDB'
 
 
+const port = normalizePort(process.env.PORT || '7000');
 
 const setupApp = async () => {
     const db = await MongoDb.getDb();
@@ -43,10 +44,47 @@ const setupApp = async () => {
 
     app.use('/', rootRouter);
 
-    var server = app.listen(3003);
+    var server = app.listen(port);
     console.log('Servidor Express iniciado na porta %s', server.address().port);
 }
 
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            log.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            log.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
 
 
 setupApp()
